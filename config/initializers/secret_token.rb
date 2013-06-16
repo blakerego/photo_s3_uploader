211122653ddfed759192aec6823cdb099760eaa3
Blake Rego <blake.rego@gmail.com>
2013-06-16 16:50:09 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoS3Uploader::Application.config.secret_token = '65a36aec3efe5fb3a5b0409098ed618735112428c60f82e6e3a30f485344ae5a3cfce90c890dd704d1b1c793acebffba5261073042c5e551c2aa705663e23e03'
